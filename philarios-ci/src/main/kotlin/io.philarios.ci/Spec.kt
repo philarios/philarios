8c9ce7cf42490385539912881e37b662463ff5e8
Marc Brandenburg <github@marcbrandenburg.net>
2019-03-12 20:59:12 +0900

    Check gradle in CI

--- philarios-ci/src/main/kotlin/io.philarios.ci/Spec.kt
+++ philarios-ci/src/main/kotlin/io.philarios.ci/Spec.kt
@@ -2,24 +2,37 @@ package io.philarios.ci
 
 import io.philarios.circleci.CheckoutStepSpec
 import io.philarios.circleci.CircleCISpec
+import io.philarios.circleci.JobBuilder
 import io.philarios.circleci.RunStepSpec
 
 val spec = CircleCISpec<Any?> {
     version("2")
 
-    jobs("build") {
-        docker {
-            image("circleci/ruby:2.4.1")
-        }
-        step(CheckoutStepSpec {
-            checkout {
-                path(".")
-            }
-        })
-        step(RunStepSpec {
-            run {
-                command("echo \"A first hello\"")
-            }
-        })
+    jobs("check") {
+        gradleDocker()
+        checkout()
+        run("gradle check")
     }
+}
+
+private fun JobBuilder<Any?>.gradleDocker() {
+    docker {
+        image("gradle:4.10.3")
+    }
+}
+
+private fun JobBuilder<Any?>.checkout() {
+    step(CheckoutStepSpec {
+        checkout {
+            path(".")
+        }
+    })
+}
+
+private fun JobBuilder<Any?>.run(command: String) {
+    step(RunStepSpec {
+        run {
+            command(command)
+        }
+    })
 }
