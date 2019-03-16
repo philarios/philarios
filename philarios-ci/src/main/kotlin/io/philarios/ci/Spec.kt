faa6bf167e85add0b98cc2781133212d78c49fa0
Marc Brandenburg <github@marcbrandenburg.net>
2019-03-16 13:13:57 +0900

    Upload test reports for all modules

--- philarios-ci/src/main/kotlin/io/philarios/ci/Spec.kt
+++ philarios-ci/src/main/kotlin/io/philarios/ci/Spec.kt
@@ -15,8 +15,7 @@ val spec = CircleCISpec<Any?> {
             mkdir -p ~/test-results/junit/
             find . -type f -regex ".*/build/test-results/.*xml" -exec cp {} ~/test-results/junit/ \;
 
-            mkdir -p ~/reports
-            cp -R ./philarios-core/build/reports/tests/test ~/reports/junit
+            find . -type d -maxdepth 1 -name "philarios-*" -exec bash -c "mkdir -p ./reports/{} && cp -r {}/build/reports/tests/test ./reports/{}/junit" \;
         """.trimIndent())
         store_test_results {
             path("~/test-results")
