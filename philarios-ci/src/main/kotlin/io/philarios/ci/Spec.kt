3a117f0474540e4414ff446089d01d0d404c20d6
Marc Brandenburg <github@marcbrandenburg.net>
2019-03-16 13:02:52 +0900

    Upload junit test reports as artifacts

--- philarios-ci/src/main/kotlin/io/philarios/ci/Spec.kt
+++ philarios-ci/src/main/kotlin/io/philarios/ci/Spec.kt
@@ -14,12 +14,15 @@ val spec = CircleCISpec<Any?> {
         run("report", """
             mkdir -p ~/test-results/junit/
             find . -type f -regex ".*/build/test-results/.*xml" -exec cp {} ~/test-results/junit/ \;
+
+            mkdir -p ~/reports
+            cp -R ./philarios-core/build/reports/tests/test ~/reports/junit
         """.trimIndent())
         store_test_results {
             path("~/test-results")
         }
         store_artifacts {
-            path("~/test-results/junit")
+            path("~/reports/junit")
         }
     }
     jobs("release-build") {
@@ -33,24 +36,13 @@ val spec = CircleCISpec<Any?> {
 
     workflows("snapshot") {
         jobs("snapshot-build") {
-            filters {
-                branches {
-                    only("master")
-                }
-            }
+            snapshotFilters()
         }
     }
 
     workflows("release") {
         jobs("release-build") {
-            filters {
-                branches {
-                    ignore("/.*/")
-                }
-                tags {
-                    only("/\\d+\\.\\d+\\.\\d+/")
-                }
-            }
+            releaseFilters()
         }
     }
 }
@@ -95,5 +87,24 @@ private fun JobBuilder<Any?>.run(name: String, command: String) {
 private fun JobBuilder<Any?>.gradleDocker() {
     docker {
         image("gradle:4.10.3")
+    }
+}
+
+private fun WorkflowJobBuilder<Any?>.snapshotFilters() {
+    filters {
+        branches {
+            only("master")
+        }
+    }
+}
+
+private fun WorkflowJobBuilder<Any?>.releaseFilters() {
+    filters {
+        branches {
+            ignore("/.*/")
+        }
+        tags {
+            only("/\\d+\\.\\d+\\.\\d+/")
+        }
     }
 }
