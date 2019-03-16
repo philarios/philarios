98882f0b9514429d8ca0d2ffa15b1d1c68b3029c
Marc Brandenburg <github@marcbrandenburg.net>
2019-03-16 13:26:27 +0900

    Add test coverage

--- philarios-ci/src/main/kotlin/io/philarios/ci/Spec.kt
+++ philarios-ci/src/main/kotlin/io/philarios/ci/Spec.kt
@@ -9,13 +9,14 @@ val spec = CircleCISpec<Any?> {
         gradleDocker()
         checkout()
         run("check", """
-            gradle check -PbintrayUser=${'$'}{BINTRAY_USER} -PbintrayKey=${'$'}{BINTRAY_KEY}
+            gradle check jacocoTestReport -PbintrayUser=${'$'}{BINTRAY_USER} -PbintrayKey=${'$'}{BINTRAY_KEY}
         """.trimIndent())
         run("report", """
             mkdir -p ~/test-results/junit/
             find . -type f -regex ".*/build/test-results/.*xml" -exec cp {} ~/test-results/junit/ \;
 
             find . -type d -maxdepth 1 -name "philarios-*" -exec bash -c "mkdir -p ~/reports/{} && cp -r {}/build/reports/tests/test ~/reports/{}/junit" \;
+            find . -type d -maxdepth 1 -name "philarios-*" -exec bash -c "mkdir -p ~/reports/{} && cp -r {}/build/reports/jacoco/test ~/reports/{}/jacoco" \;
         """.trimIndent())
         store_test_results {
             path("~/test-results")
