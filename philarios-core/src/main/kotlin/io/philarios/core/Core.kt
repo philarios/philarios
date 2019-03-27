a810e259943e51005707160c8e317010385c7e54
Marc Brandenburg <github@marcbrandenburg.net>
2019-03-27 19:39:37 +0900

    Remove spec interface

--- philarios-core/src/main/kotlin/io/philarios/core/Core.kt
+++ philarios-core/src/main/kotlin/io/philarios/core/Core.kt
@@ -3,10 +3,6 @@ package io.philarios.core
 @DslMarker
 annotation class DslBuilder
 
-interface Spec<in C, out T : Any> {
-    fun connect(context: C): Scaffold<T>
-}
-
 interface Translator<in C, out T> {
     fun translate(context: C): T
 }
