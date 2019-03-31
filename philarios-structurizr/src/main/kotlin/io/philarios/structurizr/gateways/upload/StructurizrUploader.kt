3f427d8231e43a466745d38c42c8fea0d8b2afe4
Marc Brandenburg <github@marcbrandenburg.net>
2019-03-31 21:53:52 +0900

    Change field visibilities in structurizr uploader

--- philarios-structurizr/src/main/kotlin/io/philarios/structurizr/gateways/upload/StructurizrUploader.kt
+++ philarios-structurizr/src/main/kotlin/io/philarios/structurizr/gateways/upload/StructurizrUploader.kt
@@ -5,11 +5,11 @@ import com.structurizr.api.StructurizrClient
 
 class StructurizrUploader {
 
-    val workspaceId = System.getenv("STRUCTURIZR_WORKSPACE_ID").toLong()
-    val apiKey = System.getenv("STRUCTURIZR_API_KEY")
-    val apiSecret = System.getenv("STRUCTURIZR_API_SECRET")
+    private val workspaceId = System.getenv("STRUCTURIZR_WORKSPACE_ID")!!.toLong()
+    private val apiKey = System.getenv("STRUCTURIZR_API_KEY")!!
+    private val apiSecret = System.getenv("STRUCTURIZR_API_SECRET")!!
 
-    val client = StructurizrClient(apiKey, apiSecret)
+    private val client = StructurizrClient(apiKey, apiSecret)
 
     fun upload(workspace: Workspace) {
         client.putWorkspace(workspaceId, workspace)
