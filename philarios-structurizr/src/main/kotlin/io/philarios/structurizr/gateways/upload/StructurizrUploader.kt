81984b4697e8b509ec7aa4ca11dc72d7863424ac
Marc Brandenburg <github@marcbrandenburg.net>
2019-04-03 22:02:54 +0900

    Disable structurizr workspace archiving

--- philarios-structurizr/src/main/kotlin/io/philarios/structurizr/gateways/upload/StructurizrUploader.kt
+++ philarios-structurizr/src/main/kotlin/io/philarios/structurizr/gateways/upload/StructurizrUploader.kt
@@ -9,7 +9,9 @@ class StructurizrUploader {
     private val apiKey = System.getenv("STRUCTURIZR_API_KEY")!!
     private val apiSecret = System.getenv("STRUCTURIZR_API_SECRET")!!
 
-    private val client = StructurizrClient(apiKey, apiSecret)
+    private val client = StructurizrClient(apiKey, apiSecret).apply {
+        workspaceArchiveLocation = null
+    }
 
     fun upload(workspace: Workspace) {
         client.putWorkspace(workspaceId, workspace)
