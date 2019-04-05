ae40ae52db3674629f44f39c466177f86e302366
Marc Brandenburg <github@marcbrandenburg.net>
2019-04-05 20:42:40 +0900

    Clean up propagatePkg

--- philarios-schema/src/main/kotlin/io/philarios/schema/entities/codegen/SchemaFileSpecs.kt
+++ philarios-schema/src/main/kotlin/io/philarios/schema/entities/codegen/SchemaFileSpecs.kt
@@ -72,27 +72,24 @@ private val Schema.scaffolderFileSpec
                 .addTypes(types.flatMap { it.scaffolderTypeSpecs })
                 .build()
 
-// TODO decide on how hacky this is?
 private fun Schema.propagatePkg(): Schema {
-    return copy(
-            types = types.map { it.propagatePkg(pkg) }//,
-//            references = references.map { it.propagatePkg() }
-    )
+    return copy(types = types.map { it.propagatePkg(pkg) })
 }
 
 private fun <T : Type> T.propagatePkg(pkg: String): T {
     return when (this) {
-        is Struct -> this.copy(pkg = this.pkg
-                ?: pkg, fields = fields.map { it.copy(type = it.type.propagatePkg(pkg)) }) as T
-        is Union -> this.copy(pkg = this.pkg ?: pkg, shapes = shapes.map { it.propagatePkg(pkg) }) as T
-        is EnumType -> this.copy(pkg = this.pkg ?: pkg) as T
-        is RefType -> this.copy(pkg = this.pkg ?: pkg) as T
-        is OptionType -> this.copy(type = type.propagatePkg(pkg)) as T
-        is ListType -> this.copy(type = type.propagatePkg(pkg)) as T
-        is MapType -> this.copy(keyType = keyType.propagatePkg(pkg), valueType = valueType.propagatePkg(pkg)) as T
+        is Struct -> copy(pkg = this.pkg?: pkg, fields = fields.map { it.propagatePkg(pkg) })
+        is Union -> copy(pkg = this.pkg ?: pkg, shapes = shapes.map { it.propagatePkg(pkg) })
+        is EnumType -> copy(pkg = this.pkg ?: pkg)
+        is RefType -> copy(pkg = this.pkg ?: pkg)
+        is OptionType -> copy(type = type.propagatePkg(pkg))
+        is ListType -> copy(type = type.propagatePkg(pkg))
+        is MapType -> copy(keyType = keyType.propagatePkg(pkg), valueType = valueType.propagatePkg(pkg))
         else -> this
-    }
+    } as T
 }
+
+private fun Field.propagatePkg(pkg: String): Field = copy(type = type.propagatePkg(pkg))
 
 private fun Schema.buildTypeRefs(): Map<RefType, Type> {
     return types
