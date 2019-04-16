136a95112ce95b4e7e929483c51ee3c76e8c5f40
Marc Brandenburg <github@marcbrandenburg.net>
2019-04-16 20:56:48 +0900

    Add unaryPlus to Builder interface

--- philarios-core/src/main/kotlin/io/philarios/core/Builder.kt
+++ philarios-core/src/main/kotlin/io/philarios/core/Builder.kt
@@ -6,16 +6,21 @@ annotation class DslBuilder
 interface Spec<B> {
     val body: B.() -> Unit
 }
+typealias Body<B> = B.() -> Unit
 
 interface Builder<S : Spec<B>, B : Builder<S, B>> {
     val genericContext: GenericContext
         get() = NopGenericContext
 
+    operator fun S.unaryPlus() = plus(this)
+
+    operator fun Body<B>.unaryPlus() = plus(this)
+
     operator fun plus(spec: S) {
         plus(spec.body)
     }
 
-    operator fun plus(body: B.() -> Unit) {
+    operator fun plus(body: Body<B>) {
         this.let { it as? B }?.let(body)
     }
 }
